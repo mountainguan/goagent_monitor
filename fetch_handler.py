9f854a164d73d3565d4af8d81ce8a6acf577c096
wwqgtxx <wwqgtxx@gmail.com>
2013-06-07 10:06:20 +0800

    add index page

--- fetch_handler.py
+++ fetch_handler.py
@@ -55,4 +55,3 @@ def defer_fetch(url, cluster_id, is_list=False):
 app = webapp2.WSGIApplication([
     ('/start_fetch', FetchHandler)
 ], debug=True)
-
--- index_handler.py
+++ index_handler.py
@@ -5,16 +5,7 @@ from google.appengine.api import urlfetch
 from google.appengine.api import memcache
 
 
-import json
-
-
-from fetch_config import config as fetch_config
-import lib
-
-
-
-
-class PageHandler(webapp2.RequestHandler):
+class IndexHandler(webapp2.RequestHandler):
 	def get(self, cluster_id=None):
 		PAGE0 = '''
 <html>
@@ -59,12 +50,6 @@ body {
 '''
 		PAGE2 = ''' 
 </h1>
-<table width="900" border="1" cellpadding="1" cellspacing="0" bordercolor="#000000">
-  <tr>
-    <td><table width="
-'''
-		PAGE3 = '''
-%" cellpadding="1" cellspacing="0" bgcolor="#00CC00">
       <tr>
         <td>&nbsp;</td>
       </tr>
@@ -74,7 +59,7 @@ body {
   <tr>
     <td colspan="3" align="center" valign="top">
 '''
-		PAGE4 = '''
+		PAGE3 = '''
 </td>
   </tr>
 </table>
@@ -85,40 +70,15 @@ body {
 </html>
 
 '''
-		cluster_attrs = fetch_config[cluster_id]
+		name = "GoAgent流量监控器"
+		message = "详见：https://github.com/wwqgtxx/goagent_monitor"
 
-		result = urlfetch.fetch(cluster_attrs['url'])
-		urltype = cluster_attrs['urltype']
-		name = cluster_attrs['name'].encode('utf8')
-		message = cluster_attrs['message'].encode('utf8')
-		if (urltype) == 'ini':
-			appids = lib.getAppidFromINI(result.content)
-		elif (urltype) == 'txt':
-			appids = result.content.split('|')
-
-
-		appid_dict = memcache.get_multi(appids)
-
-		response_dict = {
-			"B_available": [],
-			"C_over_quota": [],
-		}
-
-		for appid, val in appid_dict.iteritems():
-			if val is True:
-				response_dict['B_available'].append(appid)
-			elif val is False:
-				response_dict['C_over_quota'].append(appid)
-
-		response_dict['A_status_msg'] = "今日还剩 %dGB/%dGB 流量" % (len(response_dict['B_available']), len(appids))
-
-		#response_json = json.dumps(response_dict, ensure_ascii=False)
-		PAGE = PAGE0+ name + PAGE1 + name + '	' + response_dict['A_status_msg'] + PAGE2 + '%f'%((float(len(response_dict['B_available']))/float(len(appids)))*100) + PAGE3 + message + PAGE4
+		PAGE = PAGE0+ name + PAGE1 + name + PAGE2 + message + PAGE3
 
 		self.response.write(PAGE)
 
 
 app = webapp2.WSGIApplication([
 	# (r'/api', ApiHandler),
-	(r'/page/(.*)', PageHandler)
+	(r'/', IndexHandler)
 ], debug=True)
