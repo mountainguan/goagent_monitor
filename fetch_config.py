79a7c263607ca57ca56009b1ddcc341dfcb958e4
wwqgtxx <wwqgtxx@gmail.com>
2013-06-23 16:39:36 +0800

    update url

--- fetch_config.py
+++ fetch_config.py
@@ -4,8 +4,7 @@ config = {
 	# cluster_id
 	"wwqgtxx-goagent": {
 		"name": u"wwqgtxx-goagent翻墙包",  # cluster_name
-		# "url": "https://wwqgtxx-goagent.googlecode.com/git/Appid.txt", # get appids from this url
-		"url": "http://goagent.wwqgtxx-goagent.googlecode.com/git/goagent-local/proxy.ini",  # get appids from this url
+		"url": "https://raw.github.com/greatagent/ga/master/goagent-local/proxy.ini",  # get appids from this url
 		"urltype": "ini",  # this url type(ini or txt)
 		"message": "wwqgtxx-goagent",  #the message show in your web site
 		"email" : "wwqgtxx@gmail.com", #the message send to your email
